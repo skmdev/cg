45c757a58e6bcd99234a395f72029fe6615fed57
skmdev <skmdev29@gmail.com>
2019-11-28 15:58:05 +0800

    fix: get protocol bug

--- src/client/utils/index.ts
+++ src/client/utils/index.ts
@@ -1,5 +1,5 @@
 export function getProtocol(protocol: string) {
   const withSecure = process.env.NODE_ENV === 'production';
 
-  return protocol + withSecure ? 's' : '';
+  return protocol + (withSecure ? 's' : '');
 }
